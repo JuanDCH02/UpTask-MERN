4cb5e5b88a1b938b96f46bf8b5b7fd44150266dc
Juan David <juandclementeh@gmail.com>
2025-08-02 20:59:59 -0300

    crud de proyectos funcionando

--- Backend/src/controllers/ProjectController.ts
+++ Backend/src/controllers/ProjectController.ts
@@ -13,7 +13,54 @@ export class ProjectController {
             console.log(error)
         }
     }
+
     static GetAllProjects = async (req: Request, res: Response) => {
+        try {
+            const projects = await Project.find({})
+            res.json(projects)
+        } catch (error) {
+            console.log(error)
+        }
         res.send('obteniendo proyectos')
-}
+    }
+
+    static GetProjectById = async (req: Request, res: Response) => {
+        try {
+            const project = await Project.findById(req.params.id)
+            if(!project) {
+                return res.status(404).json({error: 'Proyecto no encontrado'})
+            } 
+            res.json(project)
+        } catch (error) {
+            console.log(error)
+        }
+        res.send('obteniendo proyectos')
+    }
+
+    static UpadateProject = async (req: Request, res: Response) => {
+        try {
+            const project = await Project.findByIdAndUpdate(req.params.id, req.body)
+            if(!project) {
+                return res.status(404).json({error: 'Proyecto no encontrado'})
+            } 
+          await project.save()
+            res.send('proyecto actualizado')
+        } catch (error) {
+            console.log(error)
+        }
+        res.send('obteniendo proyectos')
+    }
+
+    static DeleteProject = async (req: Request, res: Response) => {
+        try {
+            const project = await Project.findById(req.params.id)
+            if(!project) {
+                return res.status(404).json({error: 'Proyecto no encontrado'})
+            } 
+            await project.deleteOne()
+            res.send('Proyecto eliminado')
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
--- Backend/src/routes/ProjectRoutes.ts
+++ Backend/src/routes/ProjectRoutes.ts
@@ -1,11 +1,51 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
-
+import { body, param } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
-router.post('/', ProjectController.CreateProject)
+
+router.post('/', 
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripcion es obligatoria'),
+    handleInputErrors,
+    ProjectController.CreateProject)
+
 router.get('/', ProjectController.GetAllProjects)
 
+router.get('/:id', 
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripcion es obligatoria'),
+    param('id')
+        .isMongoId().withMessage('ID no valido'),
+    handleInputErrors,
+    ProjectController.GetProjectById)
+
+router.put('/:id', 
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripcion es obligatoria'),
+    param('id')
+        .isMongoId().withMessage('ID no valido'),
+    handleInputErrors,
+    ProjectController.UpadateProject)
+
+router.delete('/:id',
+    param('id')
+        .isMongoId().withMessage('ID no valido'),
+    handleInputErrors,
+    ProjectController.DeleteProject)
 
 
 
